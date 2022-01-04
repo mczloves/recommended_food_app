5f228e71c25fbf69345366941eb6a776579de0b8
mczloves <mczloves1@yahoo.co.jp>
2022-01-04 13:13:05 +0900

    Rails応用課題6

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
-User.create!(email: "test@example.com", password: "password")
-puts "ユーザーの初期データインポートに成功しました。"
+ActiveRecord::Base.connection.execute("TRUNCATE TABLE users RESTART IDENTITY CASCADE")
+ActiveRecord::Base.connection.execute("TRUNCATE TABLE foods RESTART IDENTITY CASCADE")
+# 以下，開発用の初期データ
+user1 = User.create!(email: "satou@example.com", password: "password")
+user2 = User.create!(email: "suzuki@example.com", password: "password")
+user3 = User.create!(email: "tanaka@example.com", password: "password")
+
+user2.foods.create!(name: "沖縄そば")
+user1.foods.create!(name: "海ぶどう")
+user3.foods.create!(name: "ゴーヤーチャンプルー")
+user3.foods.create!(name: "サーターアンダギー")
+user1.foods.create!(name: "タコライス")
+
+puts "初期データの投入に成功しました！"
