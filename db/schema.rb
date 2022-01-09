c4270d7ccaa13f7dcc13e18b3b3e0568f3df18ae
mczloves <mczloves1@yahoo.co.jp>
2022-01-09 15:44:37 +0900

    Rails課題７

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,8 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_29_034212) do
-
+ActiveRecord::Schema.define(version: 20_220_109_060_335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_12_29_034212) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
